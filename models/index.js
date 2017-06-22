2948adb3299d82d91afcf3eacdb898db900d5688
India Amos <india@indiamos.com>
2017-06-22 18:23:48 -0400

    Add Restaurant and Activity schemas

--- models/index.js
+++ models/index.js
@@ -1,40 +1,66 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost/tripplanner');
 
-
 var Hotel = db.define('hotel', {
   name: {
     type: Sequelize.STRING,
     allowNull: false
   },
   place: {
-    // type: Sequelize.STRING,
-    // allowNull: false
     address: Sequelize.STRING,
     city: Sequelize.STRING,
     state: Sequelize.STRING,
     phone: Sequelize.STRING,
     location: Sequelize.ARRAY,
-    cuisine: Sequelize.STRING,
-    price: Sequelize.INTEGER
   },
   num_stars: {
     type: Sequelize.FLOAT,
-    allowNull: false,
+    allowNull: false
   },
   amenities: {
+    type: Sequelize.STRING
+  };
+
+var Restaurant = db.define('restaurant', {
+  name: {
     type: Sequelize.STRING,
-
+    allowNull: false
+  },
+  place: {
+    address: Sequelize.STRING,
+    city: Sequelize.STRING,
+    state: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    location: Sequelize.ARRAY,
+  },
+  cuisine: {
+    type: Sequelize.STRING
+  },
+  price: {
+    type: Sequelize.INTEGER
   }
+}
 
-
-
-
-
-
-
+var Activity = db.define('activity', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  place: {
+    address: Sequelize.STRING,
+    city: Sequelize.STRING,
+    state: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    location: Sequelize.ARRAY
+  },
+  age_range: {
+    type: Sequelize.STRING
+  }
 })
 
-
-
-
+module.exports = {
+  db: db,
+  Restaurant: Restaurant,
+  Hotel: Hotel,
+  Activity: Activity
+};
