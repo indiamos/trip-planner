9c32f1695ff92f5cb692eef04de83e99b133cdb3
Karina Yang <karinakeen@icloud.com>
2017-06-23 11:00:52 -0400

    seeded

---
+++ models/hotel.js
@@ -0,0 +1,23 @@
+const db = require('./db');
+const Sequelize = require('sequelize');
+
+
+
+var Hotel = db.define('hotel', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  num_stars: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      min: 1, max: 5
+    }
+  },
+  amenities: {
+    type: Sequelize.STRING
+  }
+});
+
+module.exports = Hotel;
--- seed.js
+++ seed.js
@@ -3,10 +3,16 @@
 
 var Promise = require('bluebird');
 var db = require('./models').db;
-var Place = require('./models/place');
-var Hotel = require('./models/hotel');
-var Restaurant = require('./models/restaurant');
-var Activity = require('./models/activity');
+
+var Place = require('./models').Place;
+var Hotel = require('./models').Hotel;
+var Restaurant = require('./models').Restaurant;
+var Activity = require('./models').Activity;
+
+// var Place = require('./models/place');
+// var Hotel = require('./models/hotel');
+// var Restaurant = require('./models/restaurant');
+// var Activity = require('./models/activity');
 
 var data = {
   hotels: [
